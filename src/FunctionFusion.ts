e07355d60b304f0ece4e7c5b70c639ac22bca3dc
jzlai <jun-zhe.lai@spring-media.de>
2020-05-23 16:01:42 +0200

    refactor functionFusion

--- src/FunctionFusion.ts
+++ src/FunctionFusion.ts
@@ -1,5 +1,8 @@
-import { FusionConfiguration, LambdaType } from './types'
-import Lambda, { ClientConfiguration, InvocationRequest } from 'aws-sdk/clients/lambda'
+import { FusionConfiguration, LambdaType, InvokeParams } from './types'
+import Lambda, {
+  ClientConfiguration,
+  InvocationRequest,
+} from 'aws-sdk/clients/lambda'
 import { Context, Callback, Handler } from 'aws-lambda'
 import { PromiseResult } from 'aws-sdk/lib/request'
 import { AWSError } from 'aws-sdk/lib/error'
@@ -7,30 +10,52 @@ import { AWSError } from 'aws-sdk/lib/error'
 class FunctionFusion {
   fusionConfiguration: FusionConfiguration
   clientConfiguration: ClientConfiguration
+  dirName: string
 
-  constructor (fusionConfiguration: FusionConfiguration, clientConfiguration: ClientConfiguration) {
+  constructor(
+    fusionConfiguration: FusionConfiguration,
+    clientConfiguration: ClientConfiguration,
+    dirName: string
+  ) {
     this.fusionConfiguration = fusionConfiguration
     this.clientConfiguration = clientConfiguration
+    this.dirName = dirName
   }
 
-  async invokeFunctionAsync (sourceName: string, destination: LambdaType, context: Context, ...args: any[]) {
-    return this.invoke(sourceName, destination, context, 'Event', args)
+  async invokeFunctionAsync(params: InvokeParams, ...args: any[]) {
+    return this.invoke(params, 'Event', args)
   }
 
-  async invokeFunctionSync (sourceName: string, destination: LambdaType, context: Context, ...args: any[]) {
-    return this.invoke(sourceName, destination, context, 'RequestResponse', args)
+  async invokeFunctionSync(params: InvokeParams, ...args: any[]) {
+    return this.invoke(params, 'RequestResponse', args)
   }
 
-  private async invoke (sourceName: string, destination: LambdaType, context: Context, InvocationType: 'Event' | 'RequestResponse' | 'DryRun', args: any[]) {
-    if (!this.areInSameFusionGroup(sourceName, destination.name)) {
-      console.log(`Source "${sourceName}" and destination "${destination.name}" not in same lambda group. Invoking remote request.`)
+  private async invoke(
+    params: InvokeParams,
+    InvocationType: 'Event' | 'RequestResponse' | 'DryRun',
+    args: any[]
+  ) {
+    const { source, target, context } = params
+    if (!this.areInSameFusionGroup(params.source, params.target)) {
+      console.log(
+        `Source "${source}" and destination "${target}" not in same lambda group. Invoking remote request.`
+      )
       const lambda = new Lambda(this.clientConfiguration)
+      console.log(`Looking for entry for ${target}`)
+      const entry = this.fusionConfiguration.find((config) =>
+        config.lambdas.includes(target)
+      )?.entry
+
+      if (!entry) {
+        throw Error('No entry lambda found')
+      }
+
       const params: InvocationRequest = {
-        FunctionName: destination.name,
+        FunctionName: entry,
         InvocationType,
         Payload: {
-          args
-        }
+          args,
+        },
       }
       params.Payload = JSON.stringify(params.Payload)
       try {
@@ -42,17 +67,20 @@ class FunctionFusion {
         throw err
       }
     } else {
-      console.log(`Source "${sourceName}" and destination "${destination.name}" in same lambda group. Invoking local request.`)
+      console.log(
+        `Source "${source}" and destination "${target}" in same lambda group. Invoking local request.`
+      )
 
-      if (!destination.handler) {
-        throw new Error('Destination handler cannot be called')
-      }
+      const { handler } = require(`${this.dirName}/${target}`)
 
-      let res: Partial<PromiseResult<Lambda.InvocationResponse, AWSError>> = {} as any
+      let res: Partial<PromiseResult<
+        Lambda.InvocationResponse,
+        AWSError
+      >> = {} as any
       const cb = (err: Error | string | null | undefined, result: any) => {
         res = this.generateResponse(err, result)
       }
-      const directResponse = await destination.handler({ args }, context, cb)
+      const directResponse = await handler({ args }, context, cb)
       if (directResponse) {
         return this.generateResponse(null, directResponse)
       }
@@ -60,67 +88,88 @@ class FunctionFusion {
     }
   }
 
-  private generateResponse (err: Error | string | null | undefined, result: any) {
-    let res: Partial<PromiseResult<Lambda.InvocationResponse, AWSError>> = {} as any
+  private generateResponse(
+    err: Error | string | null | undefined,
+    result: any
+  ) {
+    let res: Partial<PromiseResult<
+      Lambda.InvocationResponse,
+      AWSError
+    >> = {} as any
     if (!err) {
       res = {
         StatusCode: 200,
-        Payload: result
+        Payload: result,
       }
     } else {
       res = {
         StatusCode: 200,
-        FunctionError: 'Unhandled'
+        FunctionError: 'Unhandled',
       }
       if (typeof err === 'string') {
         res.Payload = {
           errorType: 'Error',
           errorMessage: err,
-          trace: []
+          trace: [],
         }
       } else {
         res.Payload = {
           errorType: 'Error',
           errorMessage: err.message,
-          trace: err.stack
+          trace: err.stack,
         }
       }
     }
     return res
   }
 
-  private areInSameFusionGroup (sourceName: string, destinationName: string) {
-    return this.fusionConfiguration.some(fusionGroup => {
-      return fusionGroup.lambdas.includes(sourceName) && fusionGroup.lambdas.includes(destinationName)
-    })
+  private areInSameFusionGroup(sourceName: string, destinationName: string) {
+    return (
+      this.fusionConfiguration.some((fusionGroup) => {
+        return (
+          fusionGroup.lambdas.includes(sourceName) &&
+          fusionGroup.lambdas.includes(destinationName)
+        )
+      }) || sourceName === undefined
+    )
   }
 }
 
 export default FunctionFusion
 
-
 type HandlerWrapperArgs = {
-  event: Event,
-  context: Context,
-  callback: Callback,
-  handler: Handler,
-  traceId?: string,
+  event: Event
+  context: Context
+  callback: Callback
+  handler: Handler
+  traceId?: string
   lambdaName?: string
 }
-export const handlerWrapper = async ({ event, context, callback, handler, traceId, lambdaName }: HandlerWrapperArgs) => {
-  console.log({
-    traceId,
-    starttime: Date.now(),
-    lambdaName
-  })
+export const handlerWrapper = async ({
+  event,
+  context,
+  callback,
+  handler,
+  traceId,
+  lambdaName,
+}: HandlerWrapperArgs) => {
+  console.log(
+    JSON.stringify({
+      traceId,
+      starttime: Date.now(),
+      lambdaName,
+    })
+  )
 
   const result = await handler(event, context, callback)
 
-  console.log({
-    traceId,
-    endtime: Date.now(),
-    lambdaName
-  })
-  
+  console.log(
+    JSON.stringify({
+      traceId,
+      endtime: Date.now(),
+      lambdaName,
+    })
+  )
+
   return result
 }
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,19 @@
-import { Handler } from 'aws-lambda'
+import { Handler, Context } from 'aws-lambda'
 
 export type FusionConfiguration = FusionGroup[]
 
 export type FusionGroup = {
-  entry: string;
-  lambdas: string[];
+  entry: string
+  lambdas: string[]
 }
 
 export type LambdaType = {
-  name: string;
-  handler?: Handler;
+  name: string
+  handler?: Handler
+}
+
+export type InvokeParams = {
+  source: string
+  target: string
+  context: Context
 }
