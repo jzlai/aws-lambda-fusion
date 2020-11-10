0d577199c4d01a41f0565faf247d571d5a6723aa
jzlai <jun-zhe.lai@spring-media.de>
2020-11-10 20:57:55 +0100

    use only 1 github action

--- src/copyGithubActions.ts
+++ src/copyGithubActions.ts
@@ -1,14 +1,8 @@
 import cpFile from 'cp-file'
 
 export const copyGithubActions = async () => {
-  return Promise.all([
-    cpFile(
-      'node_modules/aws-lambda-fusion/dist/deploy.yml',
-      '.github/workflows/deploy.yml'
-    ),
-    cpFile(
-      'node_modules/aws-lambda-fusion/dist/deploy-stg.yml',
-      '.github/workflows/deploy-stg.yml'
-    ),
-  ])
+  return cpFile(
+    'node_modules/aws-lambda-fusion/dist/deploy.yml',
+    '.github/workflows/deploy.yml'
+  )
 }
